7bfb057623a03258ea43f7d43aca97ae128229b8
Etherdancer <85870757+Etherdancer@users.noreply.github.com>
2026-06-03 21:23:59 +0200

    chore: remove F-Droid scanner offenders

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,13 +22,11 @@ dependencyResolutionManagement {
             }
         }
         mavenCentral()
-        maven { url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") }
         maven { url = uri("https://jitpack.io") }
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "AetherWorks"
